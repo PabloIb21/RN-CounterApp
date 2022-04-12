dd1eec4baaec36bfaa735ad2e29b8edfd605b09f
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-04-12 11:31:11 -0500

    counter app

---
+++ src/screens/ContadorScreen.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Fab } from '../components/Fab';
+
+export const ContadorScreen = () => {
+  const [contador, setContador] = useState(10);
+
+  return (
+    <View style={ styles.container }>
+
+      <Text style={ styles.title }>
+        Contador: { contador }
+      </Text>
+
+      <Fab
+        title="+1"
+        onPress={() => setContador(contador + 1)}
+      />
+
+      <Fab
+        title="-1"
+        onPress={() => setContador(contador - 1)}
+        position="bl"
+      />
+
+      {/* <TouchableOpacity
+        style={ styles.fabLocationBL }
+        onPress={() => setContador(c => c - 1)}
+      >
+        <View style={ styles.fab }>
+          <Text style={ styles.fabText }>-1</Text>
+        </View>
+      </TouchableOpacity> */}
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    top: -15,
+  },
+});
---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const HolaMundoScreen = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Text style={{
+        fontSize: 45,
+      }}>Hello World</Text>
+    </View>
+  );
+};
